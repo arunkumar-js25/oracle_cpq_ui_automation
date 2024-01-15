0bc8ceab88afe87257661391574688ee13f1776b
arunkumar-js25 <arunkumar.js25@gmail.com>
2024-01-15 22:24:05 +0530

    Updated Tests

--- tests/test01.py
+++ tests/test01.py
@@ -2,7 +2,7 @@
 
 import oracle_cpq_ui_automation
 
-main = oracle_cpq_ui_automation("testnovgp","karun","Birlasoft@6")
+main = oracle_cpq_ui_automation("XXX","XXX","XXX")
 main.launchChrome()
 main.login()
 main.createGroupsinPartnerOrg([["Partner User","partnerUser",True]],[],-1)
